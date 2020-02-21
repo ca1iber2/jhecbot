5d51c13f3be2d2ce18e6a12bf8d7e4ef94bb336b
ca1iber2 <59156489+ca1iber2@users.noreply.github.com>
2020-02-20 20:35:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client()
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
-
+const userId = message.guild.members.find(m => m.id === "200413880843894785");
 client.on('message', msg => {
   if (msg.content.toLowerCase().includes('send reminder')) {
     msg.channel.sendMessage('hey everyone! reminder that practice is today from 8 to 10pm in the SDS room')
@@ -17,7 +17,11 @@ client.on('message', msg => {
   else if (msg.content.toLowerCase().includes('emma')) {
     msg.channel.sendMessage('thats fantastic')
   }
- 
+  if(!msg.sender === userID) {
+
+  if(msg.content === 'psst') {
+    msg.channel.sendMessage('Hello there!');
+  }
 })
 
 
