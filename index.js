6cf3d682854a77e4e884f6d8c045661bfb088397
ca1iber2 <59156489+ca1iber2@users.noreply.github.com>
2020-02-20 20:19:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
-
+const userId = message.guild.members.find(m => m.id === "201460893341450241");
 client.on('message', msg => {
   if (msg.content.toLowerCase().includes('send reminder')) {
     msg.channel.sendMessage('hey everyone! reminder that practice is today from 8 to 10pm in the SDS room')
@@ -16,7 +17,13 @@ client.on('message', msg => {
   else if (msg.content.toLowerCase().includes('emma')) {
     msg.channel.sendMessage('thats fantastic')
   }
-  
-})
+  if(!message.sender === userID)
+{
+
+if(msg.content.toLowerCase().includes(' ')
+{
+    message.channel.send('ok boomer');
+}
+}
 
 client.login(process.env.BOT_TOKEN)
