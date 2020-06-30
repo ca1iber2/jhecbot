97c1538d2deee9bf65c438d71c59f3e2f702ddd1
ca1iber2 <59156489+ca1iber2@users.noreply.github.com>
2020-06-30 01:07:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('message', msg => {
     msg.channel.sendMessage('hey everyone! reminder that JHEC Fun Time is Wednesday from 9 to 11pm on zoom')
   }
   else if (msg.content.toLowerCase().includes('fire')) {
-    msg.channel.sendMessage('Glory be to our great lord of fire! May the everlasting flame burn for eternity and may our foes be forever vanquished by the light of our fire! Let our fire, the burning, the light casting, the life giver, look upon us and hear our thanks!')
+    msg.channel.sendMessage("Glory be to our great lord of fire! May the everlasting flame burn for eternity and may our foes be forever vanquished by the light of our fire! Let our fire, the burning, the light casting, the life giver, look upon us and hear our thanks!")
   }
   //else if (msg.content.toLowerCase().includes('emma')) {
    // msg.channel.sendMessage('01001001 00100000 01101000 01100001 01110110 01100101 00100000 01100101 01110011 01100011 01100001 01110000 01100101 01100100 00100000 01101101 01111001 00100000 01110000 01110010 01101001 01110011 01101111 01101110 00100000 01110100 01101000 01101001 01110011 00100000 01101001 01110011 00100000 01100001 00100000 01110111 01100001 01110010 01101110 01101001 01101110 01100111')
