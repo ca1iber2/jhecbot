983d3da8ab93ec88fd45a49e08d9fb9b35d3cc9e
SL <59156489+ca1iber2@users.noreply.github.com>
2022-02-03 16:08:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,28 +10,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   
-  //this part of the code uses a user id to detect when that user has sent a message and say something afterwards
-  /*if((msg.author).id == '288102907356905472') {
-      if(msg.content.includes('i love you')) {
-         msg.channel.sendMessage('I love you too camille');
-      
-         }
-      else if (msg.content.includes('i love you meatball')) {
-         msg.channel.sendMessage('I love you too camille');
-     
-      }
-  }
-  else if ((msg.author).id == '275783651504029697'){
-      if(msg.content.includes('i love you')) {
-         msg.channel.sendMessage('I love you too emma');
-      
-      }
-      else if (msg.content.includes('i love you meatball')) {
-         msg.channel.sendMessage('I love you too emma')
-       
-      }
-  }
-  */
+  
   //checks if message author is a bot
   if ((msg.author).id == '671864740942970933') {
       return;
@@ -48,29 +27,9 @@ client.on('message', msg => {
       }
   }
   else{
-  //commands to add pronoun roles
-  //doesnt work as of july 13 2020
-      if (msg.content == '!role he') {
-         msg.member.roles.add('729560821767602197')
-         msg.channel.sendMessage('role added')
-      }
-    
-      else if (msg.content == '!role she') {
-         msg.member.roles.add('729560874272030802')
-         msg.channel.sendMessage('role added')
-      }
-    
-      else if (msg.content == '!role they') {
-         msg.member.roles.add('729560854206611588')
-         msg.channel.sendMessage('role added')
-      }
-    
-      else if (msg.content == '!role red') {
-         msg.member.roles.add('735647659041357885')
-         msg.channel.sendMessage('role added')
-      }
+  
       //when send reminder is sent, bot sends reminder message
-      else if (msg.content.toLowerCase().includes('send reminder')) {
+      if (msg.content.toLowerCase().includes('send reminder')) {
          //msg.channel.sendMessage('hey everyone! reminder that practice is today from 8 to 10pm in the SDS room')
          msg.channel.sendMessage('hey everyone! reminder that JHEC Practice is Monday from 8 to 9:30pm in the Freshmen Annex and Thursday 10 to 11:30pm in the rec center')
       }
